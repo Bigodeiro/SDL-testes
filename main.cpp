485e6d7dfd75c8ced1879807f2a8232f02015362
Bigodeiro <enzobigber@gmail.com>
2022-12-04 23:43:46 -0300

    Agora eu sei oque está acontecendo

--- main.cpp
+++ main.cpp
@@ -1,46 +1,25 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <iostream>
 
-#define SCREEN_WIDTH 1280 
-#define SCREEN_HEIGHT 720
+using namespace std;
 
-int main(int argc, char** argv){
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+int main(int argc, char** argv)
+{
+    //! inicia todos os modulos do sdl (audio event etc)
+    SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    if(!window){
-        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+    //! cria a janela em que as coisas aparecem
+    int const WIDTH = 600, HEIGHT = 500, WINDOW_XPOS = 30, WINDOW_YPOS = 30;
+    SDL_Window *window = SDL_CreateWindow("Insira o titulo aqui", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if(!renderer){
-        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+    //!aqui estamos pegando a parte da janela que criamos em que relamente podemos mexer, como se fosse um canva
+    SDL_Surface *screen = SDL_GetWindowSurface(window);
 
-    bool running = true;
-    while(running){
-        SDL_Event event;
-        while(SDL_PollEvent(&event)){
-            switch(event.type){
-                case SDL_QUIT:
-                    running = false;
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 100);
-        SDL_RenderClear(renderer);
-
-        SDL_RenderPresent(renderer);
-    }
-
+    //!destruindo a jenela desocupamos o espaço na memoria e portanto encerramos o programa
+    cout << "esta rodando" << endl;
+    SDL_Delay(3000);
+    SDL_DestroyWindow(window); //? fecha a janela
+    SDL_Quit();                //? cuida da memoria
     return 0;
 }
